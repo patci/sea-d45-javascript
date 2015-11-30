6ad1fe4bf75cd8d1a14b430057f9a1e5783b8d1c
toastynerd <tylermorgan86@gmail.com>
2015-11-30 12:45:41 -0800

    start of nov 30

---
+++ week_6/nov_30_angular/inclass_code/server.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var express = require('express');
+var app = express();
+var bearsRouter = require(__dirname + '/routes/bears_routes');
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/bear_stream_dev');
+
+app.use('/api', bearsRouter);
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('server up');
+});
