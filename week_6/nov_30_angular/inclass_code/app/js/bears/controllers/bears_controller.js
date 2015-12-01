38a2e7ef5f25c9bb9917ff25a6ac5de8aa7f8a25
toastynerd <tylermorgan86@gmail.com>
2015-11-30 16:14:17 -0800

    angular GETs

--- week_6/nov_30_angular/inclass_code/app/js/bears/controllers/bears_controller.js
+++ week_6/nov_30_angular/inclass_code/app/js/bears/controllers/bears_controller.js
@@ -1,5 +1,14 @@
 module.exports = function(app) {
-  app.controller('BearsController', ['$scope', function($scope) {
-    $scope.greeting = 'There be bears in these hills';
+  app.controller('BearsController', ['$scope', '$http', function($scope, $http) {
+    $scope.bears = [];
+
+    $scope.getAll = function() {
+      $http.get('/api/bears')
+        .then(function(res) {
+          $scope.bears = res.data;
+        }, function(err) {
+          console.log(err.data);
+        });
+    }
   }]);
 };
